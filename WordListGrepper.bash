0d95b10e294082856c902101d9a7f49a7e4a02a7
Ikko Ashimine <eltociear@gmail.com>
2021-01-21 23:03:42 +0900

    ファイル読み込み処理追加

--- WordListGrepper.bash
+++ WordListGrepper.bash
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 echo "Hello! Today is beautiful day."
+
+# リストを読み込む
+ListFile="./TargetList.txt"
+if [ ! -f $ListFile ] ; then
+    echo 'Word list is not found!'
+    exit 1;
+fi
+
+# ループで空白行以外を読む
+cat $ListFile |
+while read word
+do
+    if [ -n "$word" ] ; then
+        echo $word
+        echo '*******'
+    fi
+done < $ListFile
+
+echo 'bye!'
