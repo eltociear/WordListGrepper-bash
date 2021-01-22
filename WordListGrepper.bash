270e40283d189b81db5a03b1dcec6e255d39fe5d
Ikko Ashimine <eltociear@gmail.com>
2021-01-22 10:42:22 +0900

    文言の前後にスペース入れて調整　TODO：日本語とアルファベットをモードで切り替える

--- WordListGrepper.bash
+++ WordListGrepper.bash
@@ -22,7 +22,7 @@ while read word
 do
     if [ -n "$word" ] ; then
         # grep実行し、結果出力
-        grep -rn --color=auto $word $targetDir
+        grep -rn --color=auto " $word " $targetDir
     fi
 done < $ListFile
 
